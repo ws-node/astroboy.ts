5dbf9615bf3f4cfbff3dc0e68c3c90b1d48c0b71
Big Mogician <sunaoran@youzan.com>
2018-12-10 23:45:19 +0800

    fix: problems

--- src/builders.ts
+++ src/builders.ts
@@ -37,34 +37,36 @@ function checkRouterFolders(baseRouter: string, folders: string[], ctorPath: str
       );
     }
     else {
-      if (!checkIfTsFile(path)) return;
+      if (checkIfOnlyDeclares(path)) return;
       createTsRouterFile(baseRouter, ctorPath, routerPath, path, root);
     }
   });
 }
 
-function checkIfTsFile(p: string): any {
-  return p.endsWith(".ts") && !p.includes(".d.ts");
+function checkIfOnlyDeclares(p: string): any {
+  return p.endsWith(".d.ts");
 }
 
 function createTsRouterFile(baseRouter: string, ctorPath: string, routerPath: string, path: string, urlRoot: string) {
   try {
     // 存在手动配置的router.ts，则不做处理直接退出
-    if (fs.existsSync(`${routerPath}/${path}`)) return;
+    if (path.endsWith(".ts") && fs.existsSync(`${routerPath}/${path}`)) return;
     // 尝试按照新版逻辑解析Controller
-    const controller = require(`${ctorPath}/${path.replace(".ts", "")}`);
-    // 找不到router源定义，抛错
-    if (!controller.prototype["@router"]) throw new Error("invalid router controller.");
+    const commonName = path.split(".")[0];
+    console.log(`${ctorPath}/${commonName}`);
+    const controller = require(`${ctorPath}/${commonName}`);
+    // 找不到router源定义，静默退出
+    if (!controller.prototype["@router"]) return;
     const sourceCtor = GlobalImplements.get(controller);
     // 无法解析控制器数据，则判断是老版本的Router
     if (!sourceCtor) return;
     const controllerName = routerPath === baseRouter ?
-      path.replace(".ts", "") :
-      `${routerPath.replace(`${baseRouter}/`, "").replace("/", ".")}.${path.replace(".ts", "")}`;
+      commonName :
+      `${routerPath.replace(`${baseRouter}/`, "").replace(/\//g, ".")}.${commonName}`;
     const file: string[] = [
       "// [astroboy.ts] 自动生成的代码",
       "",
-      `const CTOR = require('${`${ctorPath}/${path.replace(".ts", "")}`}');`,
+      `const CTOR = require('${`${ctorPath}/${commonName}`}');`,
       `const { buildRouter } = require("astroboy.ts");`,
       `module.exports = buildRouter(CTOR, "${controllerName}", "${urlRoot}");`
     ];
@@ -75,11 +77,7 @@ function createTsRouterFile(baseRouter: string, ctorPath: string, routerPath: st
       if (oldFile.toString() === file.join("\n")) return;
     }
     // 复写router.js文件
-    fs.appendFileSync(
-      `${routerPath}/${path.replace(".ts", ".js")}`,
-      file.join("\n"),
-      { flag: "w" }
-    );
+    fs.appendFileSync(_PATH, file.join("\n"), { flag: "w" });
   } catch (e) {
     throw e;
   }
