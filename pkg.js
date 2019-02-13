d91935076c33471fcd717ea9419290cc229030b1
Big Mogician <sunaoran@youzan.com>
2019-02-13 16:55:57 +0800

    publish 1.0.6-rc.8

--- pkg.js
+++ pkg.js
@@ -1,4 +1,3 @@
-
 const fs = require("fs");
 const { exec } = require("child_process");
 const package = require("./package.json");
@@ -8,12 +7,13 @@ const { version } = package;
 const [main, oldrc] = (version || "").split("-");
 let [___, rc = 0] = (oldrc || "").split(".");
 rc = Number(rc) + Number(rcadd);
-package.version = `${main}${!!rctokrn ? `-rc.${rc}` : ""}`
+package.version = `${main}${!!rctokrn ? `-rc.${rc}` : ""}`;
 fs.writeFileSync("./package.json", JSON.stringify(package, null, "  "));
+fs.writeFileSync("./dist/package.json", JSON.stringify(package, null, "  "));
 
-exec("npm publish", (error, stdout, stderr) => {
+exec("cd dist && npm publish", (error, stdout, stderr) => {
   if (error) {
-    package.version = `${main}${!!rctokrn ? `-${oldrc}` : ""}`
+    package.version = `${main}${!!rctokrn ? `-${oldrc}` : ""}`;
     fs.writeFileSync("./package.json", JSON.stringify(package, null, "  "));
     throw error;
   } else {
